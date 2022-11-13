70c2b6ace890fb62e60b032b6a4f2f3b86049a77
Aashish Singh <113883387+Aashishly@users.noreply.github.com>
2022-11-13 19:50:20 +0530

    Counts Digits
    Counting Numbers of Digits

--- Mathematics/countDigits.cpp
+++ Mathematics/countDigits.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-// Approach Number 1
-int countDigits_1(long long int digit){
+
+int countDigits(long long int digit){
     int count = 0;
 
     if(digit == 0)
@@ -17,19 +17,12 @@ int countDigits_1(long long int digit){
 
 }
 
-// Approach Number 2
-int countDigits_2(long long int digit){
-    if(digit/10 == 0)
-    return 1;
-    return 1 + countDigits_2(digit/10);
-}
-
 int main(void){
 
     long long int numb = 345;
 
-    cout << "Number of digits : " << countDigits_1(numb) << endl;
-    cout << "Number of digits : " << countDigits_2(numb) << endl;
+    cout << "Number of digits : " << countDigits(numb) << endl;
+  
 
     return 0;
 
